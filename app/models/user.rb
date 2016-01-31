d34f74749fd56f41f5b370d0b7988c7685ee1c6c
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-01-31 14:58:08 -0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
+  has_many :microposts, dependent: :destroy
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -61,7 +62,11 @@ class User < ActiveRecord::Base
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-  
+
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
   def downcase_email
